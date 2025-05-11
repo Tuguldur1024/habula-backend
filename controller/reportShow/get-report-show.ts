fa4b4b821aa9a4dc1654c52f2251ee252bee30e5
Bilguuntuguldur <bilguuntuguldur@chimege.com>
2025-05-11 11:13:45 +0800

    hell marry

--- controller/reportShow/get-report-show.ts
+++ controller/reportShow/get-report-show.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import ReportShowModel from "../../model/reportsShow";
 
-export const getReportShow = async (req: Request, res: Response) => {
+export const getReportShow = async (_req: Request, res: Response) => {
     try {
         const reportShow = await ReportShowModel.find();
         res.json({ reportShows: reportShow });

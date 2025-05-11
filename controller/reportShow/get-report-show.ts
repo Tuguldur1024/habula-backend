0a604f14bd0659ceb3949101a84b2fdd7535dbe1
Bilguuntuguldur <bilguuntuguldur@chimege.com>
2025-05-11 11:02:39 +0800

    add on

---
+++ controller/reportShow/get-report-show.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import ReportShowModel from "../../model/reportsShow";
+
+export const getReportShow = async (req: Request, res: Response) => {
+    try {
+        const reportShow = await ReportShowModel.find();
+        res.json({ reportShows: reportShow });
+    } catch (error) {
+        res.json({ message: error });
+    }
+}
--- router/reportShow.router.ts
+++ router/reportShow.router.ts
@@ -1,7 +1,8 @@
 import express from "express";
 
-import { AddCount, createReportShow } from "../controller/reportShow";
+import { AddCount, createReportShow, getReportShow } from "../controller/reportShow";
 
 export const reportShowRouter = express.Router();
 reportShowRouter.post("/addcount", AddCount);
 reportShowRouter.get("/create", createReportShow);
+reportShowRouter.get("/getall", getReportShow);
